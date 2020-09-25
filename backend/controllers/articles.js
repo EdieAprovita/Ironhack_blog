69bd8528775d3cb220afcd8c1ebfd2e4f69dbdc6
EdieAprovita <edieveg316@gmail.com>
2020-09-24 20:03:57 -0500

    Adding crontrollers to article

--- backend/controllers/articles.js
+++ backend/controllers/articles.js
@@ -1,5 +1,61 @@
-const Articles = require("../models/Article")
+const Article = require('../models/Article');
 
-exports.getAllArticles = async (req,res) => {
-    
-}
+exports.getAllArticles = async (req, res) => {
+  try {
+    const articles = await Article.find();
+    res.status(200).json({ articles });
+  } catch (error) {
+    res.status(400).json({ message: `${error}` });
+  }
+};
+
+exports.getArticle = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const article = await Article.findById(id);
+    res.status(200).json({ article });
+  } catch (error) {
+    res.status(400).json({ message: `${error}` });
+  }
+};
+
+exports.createArticle = async (req, res) => {
+  try {
+    const { title, article, authorName } = req.body;
+
+    const article = await Article.create({
+      title,
+      article,
+      authorName,
+    });
+  } catch (error) {
+    res.status(400).json({ message: `${error}` });
+  }
+};
+
+exports.updateArticle = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, article, authorName } = req.body;
+
+    const article = await Article.findByIdAndUpdate(id, {
+      title,
+      article,
+      authorName,
+    });
+
+    res.status(200).json({ article });
+  } catch (error) {
+    res.status(400).json({ message: `${error}` });
+  }
+};
+
+exports.deleteArticle = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await Article.findByIdAndDelete(id);
+    res.status(200).json({ message: 'deleted article' });
+  } catch (error) {
+    res.status(400).json({ message: `${error}` });
+  }
+};
